b27c1ff196b21ec982d7166e7cc836d563401179
HuggPam <hung511511@gmail.com>
2026-04-14 14:00:45 +0700

    url

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
+        callbackURL: "/auth/google/callback",
        
         scope: ['profile', 'email', 'https://www.googleapis.com/auth/photospicker.mediaitems.readonly'],
     prompt: 'consent',   
